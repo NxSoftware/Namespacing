ae05fb4941f0f6c4173fc1743e98088feb89e79d
Steve Wilford <steve@stevewilford.co.uk>
2015-03-24 19:26:44 +0000

    Initial Commit

---
+++ Namespacing/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Namespacing
+//
+//  Created by Steve Wilford on 24/03/2015.
+//  Copyright (c) 2015 Steve Wilford. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
